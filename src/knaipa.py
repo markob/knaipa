ac7dc25742c4e486b3c0644744414412b62b5935
Marko Black <marko.blck@gmail.com>
2009-12-24 02:40:58 +0200

    temporary commit with started routing

--- src/knaipa.py
+++ src/knaipa.py
@@ -1,29 +1,51 @@
 import logging
 
+import os, sys
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-def loginRequired(func):
-    """ Login Decorator - checks current user status and
-    redirects to login page if user is not logged in. """
-    
-    def wrapper(self, *args, **kw):
-        user = users.get_current_user()
-        
-        if not user:
-            logging.debug('Anonymous user tried to get access.')
-            self.redirect(users.create_login_url(self.request.uri))
-        else:
-            func(self, *args, **kw)
-    
-    return wrapper
 
+def AddLibsToPath():
+    """ Appends lib content to Python system path. """
+
+    # get project root first
+    projRoot = os.curdir
+    # append library path to system path
+    sys.path.apend(projRoot + '/libs')
+
+
+class CommandRounter(webapp.RequestHandler):
+    """ General command preprocessor(router) which gets command
+    and passes it to appropriate router. """
+
+    __handlers = {'article': ArticleController}
+
+    def getHandler(self, cmd):
+        """ Selects appropriate handler and pass request to it. """
+        try:
+            handler = self.__handlers[self.request.get('cmd')]
+        except:
+            handler = self.__handlers['invalid']
+
+        # load appropriate handler and launch it
+        __import__(handler)
+
+
+    def get(self):
+
+        return handler.get(self.request)
+    
+
+    def post(self):
+        pass
+    
 
 application = webapp.WSGIApplication(
-    [('/cmd/login', MainPage)],
-    debug=True)
+    [('/cmd/', CommandRouter)], debug=True)
 
 def main():
+    AddLibsToPath()
     run_wsgi_app(application)
 
 if __name__ == '__main__':
