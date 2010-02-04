0ad8fa0fdb7d2511f759f7301cff0f04f2be6b20
Marko Black <marko.blck@gmail.com>
2010-02-04 07:27:59 +0200

    added new app handlers

---
+++ src/libs/models/comment.py
@@ -0,0 +1,15 @@
+import logging
+
+from google.appengine.ext import db
+
+class Comment(db.Model):
+    """ Describes data model for storing articles """
+
+    # TODO: following property should be replaced by reference
+    article = db.StringProperty(required=True)
+    title = db.StringProperty(required=True)
+    description = db.StringProperty(required=True)
+    cut = db.StringProperty(required=True)
+    text = db.TextProperty(required=True)
+    #author = db.ReferenceProperty(required=True)
+    modified = db.DateTimeProperty(auto_now=True)
