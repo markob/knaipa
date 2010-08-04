58bb0d2cd711e76cbbae26b57c3196d2bbb6b244
Marko Black <marko.blck@gmail.com>
2010-08-04 15:51:22 +0300

    updated filesystem structure

--- src/apps/__init__.py
+++
@@ -1,6 +0,0 @@
-"""This file contains configs are common for all applications"""
-
-import common
-
-# Update System Path
-common.set_system_path()
--- src/libs/models/documents.py
+++
@@ -1,6 +0,0 @@
-from models.articles import Article
-
-class IndexableDocument(Article):
-    """ It's common descendant for all indexable documents.
-        It is used as workaround for appengine polymodel poor functionality. """
-    pass
