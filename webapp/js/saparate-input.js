2616945ad67641d60831ad67731c680c0cca30b5
Neformal <neformal.lviv@gmail.com>
2010-09-29 01:42:18 +0300

    Fixed some bugs with input controls.

--- webapp/js/saparate-input.js
+++ webapp/js/saparate-input.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-	var editableArea = function (){
+	var editableArea = function (initObj){
 		var _this = this;
 
 		this.makeExtended = function (){
@@ -34,31 +34,29 @@
 		 * @return inserted node
 		 */
 		this.insertElement = function(el){
-			var sel = window.getSelection();
-			var selNode = _this.sel.anchorNode;
-			var selOffset = _this.sel.anchorOffset;
+			_this.updateSelection();
+			var newElement = $(el)[0];
 
-			var newContent = ($(selNode).text().slice(0, selOffset)||'&nbsp;')+el+'<em id="insertedTemporaryElement"></em>'+($(selNode).text().slice(selOffset)||'&nbsp;');
+			var range = _this.sel.getRangeAt(0);
+			range.insertNode(newElement)
 
-			$(selNode).replaceWith(newContent);
-			selNode = $('#insertedTemporaryElement')[0].previousSibling;
-			$('#insertedTemporaryElement').remove();
+			if (newElement.nextSibling && newElement.nextSibling.nodeType == 3 && newElement.nextSibling.nodeValue=='')newElement.nextSibling.nodeValue = '\u00A0';
+			if (newElement.previousSibling && newElement.previousSibling.nodeType == 3 && newElement.previousSibling.nodeValue=='')newElement.previousSibling.nodeValue = '\u00A0';
 
-			var range = document.createRange();
-			range.setStart(selNode.nextSibling, 0);
-			range.setEnd(selNode.nextSibling, 0);
-			window.getSelection().removeAllRanges();
-			window.getSelection().addRange(range);
+			if (!newElement.nextSibling || newElement.nextSibling.tagName == "BR") $(document.createTextNode('\u00A0')).insertAfter(newElement);
+			if (!newElement.previousSibling || newElement.previousSibling.tagName == "BR") $(document.createTextNode('\u00A0')).insertBefore(newElement);
 
-			return selNode;
+			_this.setPointer(newElement.nextSibling,0);
+			return newElement;
 		}
 
-		this.init =
+		this.updateSelection = function(){
+			_this.sel = window.getSelection();
+			_this.selNode = _this.sel.anchorNode;
+			_this.selOffset = _this.sel.anchorOffset;
 
-	}
-
-	var separateInput = function (initObj){
-		var _this = this;
+//			console.log(_this.selNode,_this.selOffset);
+		}
 
 		this.setPointer = function(el, offset){
 			if (! _this.sel)_this.sel = window.getSelection();
@@ -68,56 +66,16 @@
 			range.setEnd(el, offset);
 			_this.sel.removeAllRanges();
 			_this.sel.addRange(range);
+
+			_this.updateSelection();
 		}
 
-		this.separate = function (){
-			if (_this.separator){
-				var newElements = [];
-				$(_this.selNode).text().replace (/[^;]+;?/gim,function(){
-					var newSpan = $("<span>"+arguments[0]+"</span>").insertBefore(_this.selNode.parentNode);
-					if (arguments[1] <= _this.selOffset && arguments[1]+arguments[0].length >= _this.selOffset){ _this.setPointer(newSpan[0].firstChild, _this.selOffset-arguments[1])}
-					newElements.push(newSpan);
-				})
-				_this.trigger('separate',newElements);
-
-				$(_this.selNode.parentNode).remove();
-
-				_this.sel = window.getSelection();
-				_this.selNode = _this.sel.anchorNode;
-				_this.selOffset = _this.sel.anchorOffset;
-			}
-		}
-
-		this.merge = function (){
-			_this.selNode.parentNode.nextSibling.innerHTML = _this.selNode.nodeValue + _this.selNode.parentNode.nextSibling.firstChild.nodeValue;
-			_this.setPointer(_this.selNode.parentNode.nextSibling.firstChild, _this.selOffset);
-			_this.selNode.parentNode.parentNode.removeChild(_this.selNode.parentNode);
-		}
-
-		this.removeEmptyNodes = function(){
-			if (_this.selNode && _this.selNode.parentNode.getAttribute('contenteditable') && _this.selNode.previousSibling){
-				var selectedNode = _this.selNode.previousSibling.firstChild;
-				_this.selNode.parentNode.removeChild(_this.selNode);
-				_this.setPointer(selectedNode,selectedNode.nodeValue.length)
-			} else if (_this.selNode && _this.selNode.parentNode.getAttribute('contenteditable') && _this.selNode.nextSibling && _this.selNode.nextSibling.tagName != 'BR'){
-				var selectedNode=_this.selNode.nextSibling.firstChild;
-				_this.selNode.parentNode.removeChild(_this.selNode);
-				_this.setPointer(selectedNode,0);
-			}
-		}
-
-		this.init = function (initObj){
+		this.init = function(){
 			eventCommander.apply(_this);
-			editableArea.apply(_this);
 
 			if (initObj) for (parametr in initObj) _this[parametr] = initObj[parametr]
-
 			_this.container.wrapInner('<div style="float:left;" contenteditable="true"></div>');
-			_this.editabelEl = _this.container.find('div:first'); 
-
-			if (_this.separator){
-				_this.container.addClass('separator');
-			}
+			_this.editabelEl = _this.container.find('div:first');
 
 			_this.container
 				.data('position','compact')
@@ -131,33 +89,117 @@
 							}
 						}, 10)
 					})
+			_this.editabelEl
+				.bind('keydown',function(e){
+					if (e.keyCode == 13){ //Return
+						_this.insertElement('<br/>');
+						_this.container.trigger('keydown');
+						return false;
+					}})
+				.bind('focus',function(){_this.container.css({'z-index':99});})
+				.bind('blur',function(){_this.container.css({'z-index':0});})
+		}
+		this.init();
+
+	}
+
+	var separateInput = function (initObj){
+		var _this = this;
+
+		this.separate = function (){
+				var curSelNode = _this.selNode
+				var newElements = [];
+				$(_this.selNode).text().replace (/[^;]+;?/gim,function(){
+					var newSpan = $("<span>"+arguments[0]+"</span>").insertBefore(_this.selNode.parentNode);
+					if (arguments[1] <= _this.selOffset && arguments[1]+arguments[0].length >= _this.selOffset){ _this.setPointer(newSpan[0].firstChild, _this.selOffset-arguments[1])}
+					newElements.push(newSpan);
+				})
+				_this.trigger('separate',newElements);
+
+				$(curSelNode.parentNode).remove();
+
+				_this.updateSelection();
+		}
+
+		this.merge = function (){
+			_this.selNode.parentNode.nextSibling.innerHTML = _this.selNode.nodeValue + _this.selNode.parentNode.nextSibling.firstChild.nodeValue;
+			_this.setPointer(_this.selNode.parentNode.nextSibling.firstChild, _this.selOffset);
+			_this.selNode.parentNode.parentNode.removeChild(_this.selNode.parentNode);
+		}
+
+		this.removeEmptyNodes = function(){
+			// if selected node are not textNode
+			if (_this.selNode && $(_this.selNode).text()=='' && _this.selNode.nodeType!=3 && _this.selNode.parentNode.getAttribute('contenteditable')){
+				//jump to the previous node.
+				if (_this.selNode.previousSibling){
+					var selectedNode = _this.selNode.previousSibling.firstChild;
+					_this.selNode.parentNode.removeChild(_this.selNode);
+					_this.setPointer(selectedNode,selectedNode.nodeValue.length)
+				}
+				//jump to the next node.
+				else if (_this.selNode.nextSibling && _this.selNode.nextSibling.tagName != 'BR'){
+					var selectedNode=_this.selNode.nextSibling.firstChild;
+					_this.selNode.parentNode.removeChild(_this.selNode);
+					_this.setPointer(selectedNode,0);
+				}
+				//jump inside to the last empty node.
+//				else {
+//					if (_this.selNode.childNodes.length){
+//						console.log(_this.selNode.childNodes.length,_this.selNode.childNodes[_this.selNode.childNodes.length-1] );
+//						for (var i = _this.selNode.childNodes.length-1; i > 0 ;i--){
+//							console.log(_this.selNode.childNodes[i]);
+//							if (_this.selNode.childNodes[i].nodeType == 3){
+//								_this.setPointer(_this.selNode.childNodes[i],_this.selNode.childNodes[i].nodeValue.length);
+//								return;
+//							}
+//							_this.selNode.removeChild(_this.selNode.childNodes[i]);
+//						}
+//					}
+//
+//					var text = document.createTextNode('');
+//					_this.selNode.appendChild(text);
+//					console.log(_this.selNode,_this.selNode.lastChild,_this.selNode.childNodes.length,1);
+//					_this.setPointer(_this.selNode.lastChild,0);
+//
+//				}
+			}
+		}
+
+		this.init = function (initObj){
+			editableArea.call(_this,initObj);
+
+			_this.container.addClass('separator');
 
 			_this.editabelEl[0].onpaste = function (e){_this.container.data ('isPaste', true)};
 			_this.editabelEl
 				.bind('focus',function(e) {
-						_this.sel = window.getSelection();
-						_this.selNode = _this.sel.anchorNode;
-						_this.selOffset = _this.sel.anchorOffset;
+						_this.updateSelection();
 
 						if (_this.selNode.tagName && _this.editabelEl[0].childNodes.length){
-							if (_this.editabelEl[0].lastChild.firstChild)
-								_this.setPointer(_this.editabelEl[0].lastChild.firstChild,_this.editabelEl[0].lastChild.firstChild.nodeValue.length)
-							else _this.setPointer(_this.editabelEl[0].firstChild,_this.editabelEl[0].firstChild.nodeValue.length)
-						};
+									if (_this.editabelEl[0].lastChild.lastChild && _this.editabelEl[0].lastChild.lastChild.nodeType == 3)
+										_this.setPointer(_this.editabelEl[0].lastChild.lastChild,_this.editabelEl[0].lastChild.lastChild.nodeValue.length)
+									else _this.setPointer(_this.editabelEl[0].firstChild,0)
+								};
 					})
 				.bind('keydown',function(e){
 						window.setTimeout(function(){
+							_this.updateSelection();
+
+							if (_this.selNode == _this.editabelEl){
+								var range = document.createRange();
+								range.setStart(_this.editabelEl.firstChild, 0);
+								range.setEnd(_this.editabelEl.lastChild, 0);
+								_this.sel.removeAllRanges();
+								_this.sel.addRange(range);
+								_this.updateSelection();
+							}
+
 							// If content is pasted
 							if (_this.container.data ('isPaste')){
 								_this.editabelEl.html(_this.editabelEl.text());
-								_this.sel = window.getSelection();
 								_this.setPointer(_this.editabelEl[0].firstChild, 0);
 								_this.container.data ('isPaste', false)
 							}
-
-							_this.sel = window.getSelection();
-							_this.selNode = _this.sel.anchorNode;
-							_this.selOffset = _this.sel.anchorOffset;
 
 							// Remove empty nodes
 							_this.removeEmptyNodes();
@@ -182,15 +224,7 @@
 							_this.textContent = _this.editabelEl.text();
 
 						},10);
-
-						if (e.keyCode == 13){ //Return
-							_this.insertElement('<br/>');
-							_this.container.trigger('keydown');
-							return false;
-						}
 					})
-				.bind('focus',function(){_this.container.css({'z-index':99});})
-				.bind('blur',function(){_this.container.css({'z-index':0});})
 		}
 		this.init(initObj);
 	}
